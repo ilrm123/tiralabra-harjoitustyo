74c5b725b4ec5fc99211cf4ff7043764c74b78b0
Ilari Matikainen <ilari.matikainen@rokki.net>
2022-04-02 21:38:54 +0300

    lisää testi syvyyshaulle

--- src/tests/maze_test.py
+++ src/tests/maze_test.py
@@ -7,3 +7,17 @@ class TestMazeGeneration(unittest.TestCase):
 
     def test_verkon_vierekkaiset_oikein(self):
         self.assertEqual(self.generator.graph[1], [False, 2, 5])
+
+    def test_kaikissa_solmuissa_kaydaan(self):
+        route = self.generator.random_depthfirst()
+
+        visited = []
+        realamount =[*range(1, 13, 1)]
+
+        for edge in route:
+            if edge[0] not in visited:
+                visited.append(edge[0])
+            if edge[1] not in visited:
+                visited.append(edge[1])
+
+        self.assertEqual(sorted(visited), realamount)
