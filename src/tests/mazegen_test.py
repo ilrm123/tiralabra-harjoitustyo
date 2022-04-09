79b899f77f9f4dbd06cc524239cdd392b87d5a32
Ilari Matikainen <ilari.matikainen@rokki.net>
2022-04-09 17:25:01 +0300

    korjaa testit ja lisää uusi testi

--- src/tests/mazegen_test.py
+++ src/tests/mazegen_test.py
@@ -4,12 +4,14 @@ from mazegen import MazeGeneration
 class TestMazeGeneration(unittest.TestCase):
     def setUp(self):
         self.generator = MazeGeneration(3, 4)
+        self.graph1 = self.generator.create_graph()
+        self.graph2 = self.generator.create_graph()
 
     def test_verkon_vierekkaiset_oikein(self):
-        self.assertEqual(self.generator.graph[1], [False, 2, 5])
+        self.assertEqual(self.graph1[1], [False, 2, 5])
 
-    def test_kaikissa_solmuissa_kaydaan(self):
-        route = self.generator.random_depthfirst()
+    def test_kaikissa_solmuissa_kaydaan_syvyyshaku(self):
+        route = self.generator.random_depthfirst(self.graph1)
 
         visited = []
         realamount =[*range(1, 13, 1)]
@@ -21,3 +23,18 @@ class TestMazeGeneration(unittest.TestCase):
                 visited.append(edge[1])
 
         self.assertEqual(sorted(visited), realamount)
+
+    def test_kaikissa_solmuissa_kaydaan_prim(self):
+        route = self.generator.random_prim(self.graph2)
+
+        visited = []
+        realamount =[*range(1, 13, 1)]
+
+        for edge in route:
+            if edge[0] not in visited:
+                visited.append(edge[0])
+            if edge[1] not in visited:
+                visited.append(edge[1])
+
+        self.assertEqual(sorted(visited), realamount)
+
